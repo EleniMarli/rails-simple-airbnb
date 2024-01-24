175f20c8d8ec1d6c82a6614c6c0878678d5065c1
Eleni Marli <elenimarli@yahoo.gr>
2024-01-24 16:01:30 +0100

    Implement 'As a user, I can add a flat' use story in Silo for 'Rails Simple Airbnb' challenge

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,7 +1,27 @@
 class FlatsController < ApplicationController
-
+  before_action :set_flat, only: %i[create]
   def index
     @flats = Flat.all
     @num = Flat.count
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    redirect_to flat_path(@flat)
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+
+  def set_flat
+    @flat = Flat.new(params[:id])
   end
 end
